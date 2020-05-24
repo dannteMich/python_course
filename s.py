4af1e1b261f5c7d1f3415d6c9ebf83726e35fb5d
Michael Rabinovich <michaelr@Kayhut.local>
2020-05-24 09:32:44 +0300

    initial commit

---
+++ s.py
@@ -0,0 +1,15 @@
+print("hello there")
+
+age_as_a_string = input("what is your age? ")
+age = int(age_as_a_string)
+
+print("Great. You are now")
+print(age)
+print("Next year you will be")
+print(age+1)
+
+print("Great. You are now", age, ". Next year you will be", age+1)
+
+
+
+
---
+++ types.py
@@ -0,0 +1,30 @@
+str
+phantom = "Phantom of the"
+movie = phantom + " opera"
+single_char = "c"
+single_quotes = 'hello world'
+empty_string = ''
+
+
+
+
+int
+number1 = 3
+number2 = 1002
+number2 = number2 + 1
+number3 = number1 + number2
+negative_number = -3
+zero = 0
+
+
+float
+f_number = 0.3
+f_number2 = -6.4
+another_zero = 0.0
+looks_like_int = 3.0
+
+bool
+True
+False
+
+None
