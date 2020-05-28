bb808301215448ae4fe3d83aa6cd6e7dfa3c4d4f
Michael <dannte@gmail.com>
2020-05-28 15:59:50 +0300

    some updates

--- 01_types/types.py
+++ 01_types/types.py
@@ -13,6 +13,8 @@ number1 = 3
 number2 = 1002
 number2 = number2 + 1
 number3 = number1 + number2
+num4 = number1 * 14 
+num5 = 10 - 2
 negative_number = -3
 zero = 0
 
@@ -24,7 +26,19 @@ another_zero = 0.0
 looks_like_int = 3.0
 
 bool
-True
-False
+boolean1 = True
+b2 = False
+b3 = (4 < 6)
+b43 = (3.2 >= 100.2)
 
-None
+
+nothing = None
+
+# var output
+print(-34)
+print(0.56)
+print(10+1)
+print(True)
+print("hello world")
+print(some_value)
+print("my age is", age)
