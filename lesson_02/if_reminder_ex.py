0d9183520631c12993cf6e4b3000de9fa620a280
Michael Rabinovich <michaelr@Kayhut.local>
2020-06-24 10:37:40 +0300

    added ex solution

---
+++ lesson_02/if_reminder_ex.py
@@ -0,0 +1,7 @@
+number = input("please input a 2-digit number")
+number = int(number)
+
+if number >= 10 and number <= 99:
+    print("This is a 2-digit number!!!")
+else:
+    print("This is not a 2-digit number")
