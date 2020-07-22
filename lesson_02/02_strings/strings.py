c94cf7827c5e07dae192ea25784e66f783d41f14
Michael Rabinovich <michaelr@Kayhut.local>
2020-07-22 09:25:54 +0300

    more strings

--- lesson_02/02_strings/strings.py
+++ lesson_02/02_strings/strings.py
@@ -12,3 +12,27 @@ else:
 
 string_length = len(basic_string)  # 11
 
+# concatination
+first_s = "I am first"
+second_s = "I am second"
+
+final = first_s + second_s
+print(final) # "I am firstI am Second"
+fix_final = first_s + " " + second_s
+print(fix_final) # "I am first I am Second"
+
+# the in operator
+sentence = "This is the end of the world as we know it" 
+if 'n' in sentence:
+    print("n in in the sentence")
+
+if 'N' in sentence:
+    print("n is also here")
+else:
+    print("N is in this sentence")
+
+if "now" in sentence:   # True
+    print("The sentence contains the now")
+
+if "Now" in sentence:   # False
+    print("Now is here")
