e3aa6eef08f00a090e83ec2b1faded8cab57bdfb
Michael Rabinovich <michaelr@Kayhut.local>
2020-07-26 16:18:18 +0300

    added some files

---
+++ lesson_02/hw/patbas_guess_number.py
@@ -0,0 +1,16 @@
+
+print("Casino user - you're up: What is the target number?")
+target_number = int(input())
+
+print("Player - time to play. What is you'r guess?")
+guess = int(input())
+
+while guess != target_number:
+    if guess < target_number:
+        print("The target number is bigger. ")
+    else:
+        print("The target number is smaller. ")
+
+    guess = int(input("what is your next guess"))
+
+print("Correct! the numver is", target_number)
