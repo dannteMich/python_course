facd1cec1de9cf8136d8712965d0eeff94b02520
Michael Rabinovich <michaelr@Kayhut.local>
2020-07-26 16:50:00 +0300

    finished some of the patbas

--- lesson_02/hw/patbas_char_search.py
+++ lesson_02/hw/patbas_char_search.py
@@ -1,7 +1,11 @@
 # first and last string
-to_search = input("Please enter a string")
+to_search = input("Please enter a string: ")
 
 to_search_length = len(to_search)
+if len(to_search) < 1:
+    print("this string is too shiort. Exiting")
+    exit();
+
 if to_search[0] == to_search[to_search_length-1]:
     print("First and last characters are equal")
 else:
@@ -14,9 +18,30 @@ else:
     print("First and last characters are not the same")
 
 
-#first 2 and last 2 with begative indices
-to_search = input("Please enter a string")
+#first 2 and last 2 with negative indices
+to_search = input("Please enter a string: ")
+if len(to_search) < 3:
+    print("this string is too shiort. Exiting")
+    exit();
+
 if (to_search[0] == to_search[-1]) and (to_search[1] == to_search[-2]):
     print("Equal")
 else:
     print("Not Equal")
+
+
+#is Polyndrom
+to_search = input("Please enter a string to check if polyndrom: ")
+is_polyndrom = True
+str_length = len(to_search)
+i = 0
+
+while i < str_length:
+    if to_search[i] != to_search[str_length-i-1]:
+        is_polyndrom = False
+    i = i + 1
+
+if is_polyndrom:
+    print("This is polyndrom!!!")
+else:
+    print("This is not a polyndrom")
