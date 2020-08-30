2ace83085adc3485462bd35eca1ae5df21eba073
Michael Rabinovich <michaelr@Kayhut.local>
2020-08-30 12:53:56 +0300

    added ekif slides

---
+++ lesson_03/elif.py
@@ -0,0 +1,29 @@
+
+num = int(input("enter a number"))
+
+if num >= 100:
+    print("Big ass number")
+
+elif num < 10:
+    print("This is a small number")
+
+else:
+    print("This is a middle number")
+
+
+
+num = int(input("enter a number"))
+if num >= 100:
+    print("Big ass number")
+
+if '7' in str(num):
+    print("number container the digit 7")
+
+elif num < 10:
+    print("This is a small number")
+
+elif num % 9 == 0:
+    print("Number is devided by 9")
+
+else:
+    print("This is a middle number")
